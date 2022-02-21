b1ecd791ad6729fdf38d16fbc2551436d52960b0
Park YongJune <redplus@redplus.net>
2022-02-21 14:49:32 +0900

    .

---
+++ DotNetNote/DotNetNote/Data/Migrations/20220221054706_FirstCreate.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DotNetNote.Data.Migrations
+{
+    public partial class FirstCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CabinetTypes",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Identification = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Show = table.Column<bool>(type: "bit", nullable: true),
+                    Adjusted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CabinetTypes", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CabinetTypes");
+        }
+    }
+}
